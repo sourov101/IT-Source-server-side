48e0da4cde4856cae76946d289ef6aee455005e0
sourov <sourov.jsr98@gmail.com>
2022-10-26 02:07:12 +0600

    course name with id fake data created

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+const courseName = require('./data/courseName.json');
 
 app.get('/', (req, res) => {
     res.send('API running');
 });
+
+app.get('/courseName', (req, res) => {
+    res.send(courseName);
+})
+
 
 app.listen(port, () => {
     console.log('server is running on port', port)
