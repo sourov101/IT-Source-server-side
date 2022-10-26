001570d0c47f4b6ac000dbf8ffed8848cbcf4c66
sourov <sourov.jsr98@gmail.com>
2022-10-26 13:46:06 +0600

    data send from server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const courseName = require('./data/courseName.json');
+const courseDetails = require('./data/courseDetails.json');
 
 app.get('/', (req, res) => {
     res.send('API running');
@@ -12,7 +13,14 @@ app.get('/', (req, res) => {
 
 app.get('/courseName', (req, res) => {
     res.send(courseName);
-})
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courseDetails.find(c => c.id == id)
+
+    res.send(selectedCourse);
+});
 
 
 app.listen(port, () => {
